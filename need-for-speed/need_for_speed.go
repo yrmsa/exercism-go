82f792a041db4056a4cc2697286e94158d217ae3
yrmsa <excelnut.cracker@gmail.com>
2025-07-31 22:59:06 +0700

    feat: need_for_speed exercise

---
+++ need-for-speed/need_for_speed.go
@@ -0,0 +1,50 @@
+package speed
+
+type Car struct {
+	battery, batteryDrain, speed, distance int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car {
+		battery: 100,
+		batteryDrain: batteryDrain,
+		speed: speed,
+		distance: 0,
+	}
+}
+
+type Track struct {
+	distance int
+}
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track {
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.battery >= car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	// OK but checking the maximum distance feels more right
+	// if car.battery < car.batteryDrain {
+	// 	return false
+	// }
+
+	// batteryNeeded := track.distance / car.speed * car.batteryDrain
+	// return car.battery - batteryNeeded >= 0
+
+	maximumDistance := car.battery / car.batteryDrain * car.speed
+	return maximumDistance >= track.distance
+}
