bc394f35cf7807bf376857f0e7bcc93f91c3c7f7
yrmsa <excelnut.cracker@gmail.com>
2025-08-06 21:36:45 +0700

    feat: bird_watcher exercise

---
+++ bird-watcher/bird_watcher.go
@@ -0,0 +1,37 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	count := 0
+	for i := 0; i < len(birdsPerDay); i++ {
+		count += birdsPerDay[i];
+	}
+
+	return count
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	count := 0
+
+	// get initial index to access
+	initIndex := 7 * (week - 1)
+
+	for i := initIndex; i < initIndex + 7; i++ {
+		count += birdsPerDay[i]
+	}
+
+	return count
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i] += 1
+	}
+
+	return birdsPerDay
+}
